5672445c4297766839e18b896a76f75f88b03563
sabbir1452 <sabbirrahman1452@gmail.com>
2024-01-27 19:07:46 +0600

    aldkjf

--- js/first.js
+++ js/first.js
@@ -2,3 +2,22 @@ console.log("first file");
 
 const allLi = document.getElementsByTagName("li");
 console.log(allLi);
+
+const allTitles = document.getElementsByClassName("section-title");
+console.log(allTitles);
+
+const secondSection = document.getElementById("second-section");
+console.log(secondSection);
+secondSection.style.color = "white";
+secondSection.style.backgroundColor = "tomato";
+
+// document.querySelectorAll
+// document.querySelector
+
+const secondList = document.getElementById("second-list");
+
+//
+const li = document.createElement("li");
+li.innerText = "My dynmic item";
+
+secondList.appendChild(li);
